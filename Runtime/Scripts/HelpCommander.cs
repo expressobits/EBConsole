5ea9b75163dd600ba50b7f1a32ea8161eb5b1dba
Scriptsengineer <scriptengineer@gmail.com>
2020-05-23 00:15:30 -0300

    Update README

--- Runtime/Scripts/HelpCommander.cs
+++ Runtime/Scripts/HelpCommander.cs
@@ -28,7 +28,7 @@ namespace ExpressoBits.Console
         {
             if (!m_Logs) return;
             //NOTE get package version?
-            Consoler.Instance.Logs.Log("Expresso Bits Console <color=red>v0.8.5</color>", 3f);
+            Consoler.Instance.Logs.Log("Expresso Bits Console <color=red>v0.9.0</color>", 3f);
 
             Consoler.Instance.Logs.LogHelp(helpTextToOpen);
 
